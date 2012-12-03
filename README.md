80c7b06255c658acb7ed1dc2356fc8f0c625175d
Dan Brough <dan@danbrough.org>
2012-12-03 10:04:15 -0500

    .

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ampd
-====
+=============
 
-AMPD mpd client for Android
+AMPD android mpd client for the Music player daemon:
+http://mpd.wikia.com/wiki/Music_Player_Daemon_Wiki
+
+Copyright (C) 2012 Dan Brough <dan@danbrough.org>
--- src/org/danbrough/ampd/Ampd.java
+++ src/org/danbrough/ampd/Ampd.java
@@ -1,8 +1,27 @@
+/**
+ * Ampd is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ampd is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ampd.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.danbrough.ampd;
 
 import org.danbrough.ampd.covers.CoverService;
 import org.danbrough.ampd.lyrics.LyricsService;
 
+/**
+ * 
+ * @author dan
+ * 
+ */
 public interface Ampd {
   enum MpdView {
     INFO, LIBRARY, CONTROLS, PLAYLIST, SERVERS;
